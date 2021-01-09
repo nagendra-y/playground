5054da08d2f2c23bbb8b59f44628e75fdd202fea
Nagendra <contact@nagendrawhy.com>
2021-01-09 14:45:23 +0530

    select platform and change description

--- controller.js
+++ controller.js
@@ -1,57 +1,180 @@
 var playground = angular.module('MesiboPlayground', []);
 
+playground.directive('customAutofocus', function($timeout) {
+	return{
+		restrict: 'A',
+
+		link: function(scope, element, attrs){
+			scope.$watch(function(){
+				return scope.$eval(attrs.customAutofocus);
+			},function (newValue){
+				if (newValue === true){
+					$timeout(function() {
+						element[0].focus();
+					});
+					//use focus function instead of autofocus attribute to avoid cross browser problem. And autofocus should only be used to mark an element to be focused when page loads.
+				}
+			});
+		}
+	};
+})
+
 playground.controller('AppController', ['$scope', '$window', '$anchorScroll', function ($scope, $window, $anchorScroll) {
 	$scope.root = "https://api.mesibo.com/api.php";
-	$scope.apis = [];
+	$scope.apis = [
+		{	
+			"op" : "useradd",
+			"title" : "Add a User or Regenerate a User Access Token",
+			"description": "To enable real-time communication between your users, you need to let mesibo know about each of your users. Mesibo will create an access token for each user and give it to you which you can send it to your users. Your user can then use this access token in Mesibo Real-time APIs using setAccessToken function.",
 
-	var a = {};
-	a.op = "useradd";
-	a.title = "Add a User or Regenerate a User Access Token";
-	a.description = "To enable real-time communication between your users, you need to let mesibo know about each of your users. Mesibo will create an access token for each user and give it to you which you can send it to your users. Your user can then use this access token in Mesibo Real-time APIs using setAccessToken function.";
-	var params = [];
-	params[0] = {
-		"name": "addr",
-		"description": "User Address (e.g phone number, email address, etc.)",
-		"type": "String",
-		"required": true,
-		"default": ""
-	};
-	params[1] = {
-		"name": "appid",
-		"description": "Android package name or iOS Bundle id. The generated token will be applicable for the app having this package name or Bundle id only. For all other platforms, you can pass any unique string, and the same need to be passed to setAppName function.",
-		"type": "String",
-		"required": true,
-		"default": ""
-	};
-	params[2] = {
-		"name": "expiry",
-		"description": "In minutes, default 1 year",
-		"type": "Number",
-		"required": false,
-		"default": 525600 
-	};
-	
-	params[3] = {
-		"name": "active",
-		"description": "Enable user, default 1 (active)",
-		"type": "Number",
-		"required": false,
-		"default": 1
-	};
+			"params": [ 
+				{
+					"name": "addr",
+					"description": "User Address (e.g phone number, email address, etc.)",
+					"type": "String",
+					"required": true,
+					"default": ""
+				},
+				{
+					"name": "appid",
+					"description": "Custom Application ID",
+					"alt":
+					{
+						"Android": 
+						{
+							"description": "Android Package Name. The generated token will be applicable for the app having this package name only",
+							"placeholder": "Example, com.mesibo.androidapp"
+						},
+						"iOS": 
+						{ 
+							"description": "iOS Bundle ID. The generated token will be applicable for the app having this Bundle id only",
+							"placeholder": "Example, com.mesibo.iosapp"
+						},
 
-	params[4] = {
-		"name": "groups",
-		"description": "Maximum Groups",
-		"type": "Number",
-		"required": false,
-	};
-	a.params = params;
+						"Other": {
+							"description":"In case of other platforms like Web, Desktop, etc. Pass any unique string. Ensure that same need to be passed to setAppName function",
+							"placeholder": "Example, web"
+						},
+					},
+					"platforms": ["Android", "iOS", "Other"],
+					"type": "String",
+					"required": true,
+					"default": ""
+				},
+				{
+					"name": "expiry",
+					"description": "In minutes, default 1 year",
+					"type": "Number",
+					"required": false,
+					"default": 525600 
+				},
 
-	a.response = {"OK": "Returns a user object on success"}
+				{
+					"name": "active",
+					"description": "Enable user, default 1 (active)",
+					"type": "Number",
+					"required": false,
+					"default": 1
+				},
 
-	$scope.apis[0] = a; 
+				{
+					"name": "groups",
+					"description": "Maximum Groups",
+					"type": "Number",
+					"required": false,
+				}
+			],
+
+			"response" : {"OK": "Returns a user object on success"}
+		}	
+
+	];
+
 	$scope.apis[1] = {"op":"useredit", "title": "Edit a User", "parameters": []}; 
-	$scope.apis[2] = {"op" : "userdel", "title": "Delete a User", "parameters": []}; 
-}]);
+	$scope.apis[2] = {"op" : "userdel", "title": "Delete a User", "parameters": []};
 
+	$scope.getRequestUrl = function(api){
+		if(!api)
+			return;
+
+		var root = $scope.root;
+		if(!root)
+			return;
+
+		var url = root;
+		if(api.op)
+			url = url + "&op="+ api.op;
+
+		var params = api.params;
+		if(!params)
+			return url;
+
+		for(var i=0; i<params.length; i++){
+			var param = params[i].name;
+			var value = "";
+
+			var param_value = document.getElementById("input-param-"+ param);
+			if(param_value)
+				value = param_value.value;
+
+			if(!(value == undefined || value == null || value == NaN)){
+				var pv = param + "=" + value;
+				url = url + "&" + pv;
+			}
+		}
+
+		return url; 
+	}
+
+	$scope.updateRequestUrl = function(api, param){
+		if(!(api && param))
+			return;
+
+		//Use regexp to replace param=value part in the url
+
+		var url = $scope.getRequestUrl(api);
+		if(!url)
+			return;
+
+		var eleUrl = document.getElementById("request-url-"+ api.op);
+		if(!eleUrl)
+			return;
+
+		eleUrl.value = url;
+	}
+
+	$scope.updateDescription = function(param, platform){
+		if(!(param && platform))
+			return;
+
+		if(!param.name)
+			return;
+
+		var e = document.getElementById("platform-menu-"+ param.name);
+		if(!e)
+			return;
+		e.textContent = platform;
+
+		var alt = param.alt;
+		if(!alt)
+			return;
+
+		var newDescription = alt[platform].description;
+		if(!newDescription)
+			return;
+
+		var param_description = document.getElementById("param-description-"+ param.name);
+		if(!param_description)
+			return;
+
+		param_description.innerHTML = newDescription;
+		console.log("updateDescription", newDescription, param_description);
+		
+		if(document.getElementById("input-param-"+ param.name)){
+			if(alt[platform].placeholder)
+				document.getElementById("input-param-"+ param.name).placeholder = alt[platform].placeholder;
+		}
+			
+	}
+}]);
 
